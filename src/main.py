a3b97d76b4be1c913880a2e65a3c581f6dbc4630
Ileis <gabrielileis07@gmail.com>
2025-01-10 12:12:41 -0300

    cost functions implemented

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from Graph import Graph
 from depth_first_search import depth_first_search
 from breadth_first_search import breadth_first_search
+from utils.algorithm import Moves
 
 SIZE: int = 30
 
 def main() -> None:
     graph = Graph(SIZE)
+    print(type(Moves.UP))
 
     inicio: tuple[int, int] = (5, 12)
     fim: tuple[int, int] = (12, 12)
--- src/utils/algorithm.py
+++ src/utils/algorithm.py
@@ -22,6 +22,9 @@ def scd(t: Position) -> int:
 def sum_position(t1: Position, t2: Position) -> Position:
     return (fst(t1) + fst(t2), scd(t1) + scd(t2))
 
+def diff_position(t1: Position, t2:Position) -> Position:
+    return (fst(t1) - fst(t2), scd(t1) - scd(t2))
+
 def is_visited(table_visited: list[list[bool]], t: Position) -> bool:
     return table_visited[fst(t)][scd(t)]
 
@@ -57,5 +60,27 @@ def man(t1: Position, t2: Position) -> int:
 def cost_1(parent: Node, pos: Position) -> int:
     return 10
 
-# def cost_2(parent: Node, pos: Position) -> int:
-    # return
+def cost_2(parent: Node, pos: Position) -> int:
+    mov_diff: Position = diff_position(parent.position, pos)
+
+    if mov_diff == Moves.UP.value or mov_diff == Moves.DOWN.value: return 10
+    if mov_diff == Moves.LEFT.value or mov_diff == Moves.RIGHT.value: return 15
+    return 0
+
+def cost_3(parent: Node, pos: Position) -> int:
+    mov_diff: Position = diff_position(parent.position, pos)
+
+    if mov_diff == Moves.UP.value or mov_diff == Moves.DOWN.value: return 10
+    if mov_diff == Moves.LEFT.value or mov_diff == Moves.RIGHT.value:
+        return 10 + (abs(5 - parent.height + 1) % 6)
+
+    return 0
+
+def cost_4(parent: Node, pos: Position) -> int:
+    mov_diff: Position = diff_position(parent.position, pos)
+
+    if mov_diff == Moves.UP.value or mov_diff == Moves.DOWN.value: return 10
+    if mov_diff == Moves.LEFT.value or mov_diff == Moves.RIGHT.value:
+        return 5 + (abs(10 - parent.height + 1) % 11)
+
+    return 0
