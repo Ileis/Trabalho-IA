a9ca6c6ba78e88e4c475e222163b4fddba99e2cc
Ileis <gabrielileis07@gmail.com>
2025-01-10 12:59:59 -0300

    :)

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
 from Graph import Graph
 from depth_first_search import depth_first_search
 from breadth_first_search import breadth_first_search
-from utils.algorithm import Moves, cost_1, cost_2, cost_3, cost_4
+from utils.algorithm import cost_1, cost_2, cost_3, cost_4
 from dijkstra import dijkstra
 
 SIZE: int = 30
 
 def main() -> None:
     graph = Graph(SIZE)
-    print(type(Moves.UP))
 
     inicio: tuple[int, int] = (5, 12)
     fim: tuple[int, int] = (12, 12)
 
-    # print(breadth_first_search(graph, inicio, fim, cost_1))
-    # print(depth_first_search(graph, inicio, fim))
-    print(dijkstra(graph, inicio, fim, cost_3))
-    
+    print(dijkstra(graph, inicio, fim, cost_1), end="\n\n")
+    print(dijkstra(graph, inicio, fim, cost_2), end="\n\n")
+    print(dijkstra(graph, inicio, fim, cost_3), end="\n\n")
+    print(dijkstra(graph, inicio, fim, cost_4), end="\n\n")
+
 if __name__ == '__main__':
     main()
