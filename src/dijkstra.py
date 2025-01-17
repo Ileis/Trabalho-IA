4bb3d1760c82df2c3bb5b5f4ad4184bc8646ec43
Ileis <gabrielileis07@gmail.com>
2025-01-17 15:21:23 -0300

    kwargs and docstring in dijkstra

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -4,15 +4,33 @@ from Node import Node
 from Heap import Heap
 from utils.algorithm import Position, Path, init_visited, sum_position, is_visited, set_visited, get_path
 
-def dijkstra(g: Graph, start: Position, end: Position, fun_g: Callable[[Node, Position], int]) -> Path:
+def dijkstra(g: Graph, start: Position, end: Position, fun_g: Callable[[Node, Position], int], **kwargs) -> Path:
+    """
+    ## **kwargs
+    `call`: printa a camada da chamada recursiva
+    `parent_node`: printa o no pai da chamada recursiva
+    `neighbors`: printa os vizinhos descubertos na busca
+    `structure_neighbors`: printa a estrutura em que os nós descubertos estão guardados
+    """
+
     root: Node = Node(start, g.get_moves(start))
     visited: list[list[bool]] = init_visited(g.size)
     heap: Heap[Node] = Heap(lambda x, y: x.h <= y.h)
+    call: int = 1
 
     def _dijkstra(r: Node | None) -> Node | None:
         nonlocal visited
         nonlocal heap
         nonlocal end
+        nonlocal call
+
+        if kwargs.get("call"):
+            print(f"call {call}" if call <= 1 else f"\ncall {call}")
+        
+        call += 1
+
+        if kwargs.get("parent_node"):
+            print(f"parent_node: {r}")
 
         if r is None or r.position == end:
             return r
@@ -26,6 +44,12 @@ def dijkstra(g: Graph, start: Position, end: Position, fun_g: Callable[[Node, Po
                 set_visited(visited, neighbor_position)
                 neighbor_node: Node = Node(neighbor_position, g.get_moves(neighbor_position), r, g_cost)
                 heap.insert(neighbor_node)
+
+                if kwargs.get("neighbors"):
+                    print(neighbor_node)
+
+        if kwargs.get("structure_neighbors"):
+            print(heap)
 
         return _dijkstra(heap.extract_head())
 
