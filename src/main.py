e191d4c49841f8450c5cd3b57fd94da067d142e7
Cauã Victor Pinheiro da Silva <cauanqx@alu.ufc.br>
2025-01-19 17:16:46 -0300

    Added random functionaly to graph
    abubleh

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from utils.algorithm import cost_1, cost_2, cost_3, cost_4, euc, man
 from iterative.g_a_star import a_star as g_a_star
 from iterative.a_star import a_star as a_star
 from iterative.dijkstra import dijkstra
-
+from iterative.breadth_first_search import breadth_first_search as bfs
 
 SIZE: int = 300
 sys.setrecursionlimit(5000)
@@ -19,7 +19,7 @@ def main() -> None:
     # start = (3, 12)
     # end = (2, 24)
 
-    for i in range(1):
+    for i in range(10):
         start = random_pos()
         end = random_pos()
         print("start:" if i == 0 else "\nstart:", start)
@@ -27,10 +27,10 @@ def main() -> None:
 
         print("g_a_star:")
         print(g_a_star(graph, start, end, cost_1, euc))
-        print("a_star:")
-        print(a_star(graph, start, end, cost_1, euc))
+        print("bfs:")
+        print(bfs(graph, start, end, cost_1)[3])
         print("dijkstra:")
-        print(dijkstra(graph, start, end, cost_1))
+        print(dijkstra(graph, start, end, cost_1)[3])
 
 
 if __name__ == '__main__':
