5c1173763a178a8db4c58816cb11b54d8070b7af
Ileis <gabrielileis07@gmail.com>
2025-01-18 23:06:02 -0300

    reduce removed

--- src/utils/algorithm.py
+++ src/utils/algorithm.py
@@ -24,14 +24,6 @@ class Moves(Enum):
     DOWN = (-1, 0)
     LEFT = (0, -1)
 
-def reduce(fun, l) -> int:
-    acc = 0
-
-    for i in l:
-        acc += fun(i)
-
-    return acc
-
 def fst(t: Position) -> int:
     return t[0]
 
