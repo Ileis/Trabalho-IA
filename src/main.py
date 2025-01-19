72a8f4406e4d011ee7beb38b0389f7eb90398775
Ileis <gabrielileis07@gmail.com>
2025-01-19 17:16:47 -0300

    generated verification fixed

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
-import sys
 import random
 from Graph import Graph
 from utils.algorithm import cost_1, cost_2, cost_3, cost_4, euc, man
-from iterative.g_a_star import a_star as g_a_star
-from iterative.a_star import a_star as a_star
-from iterative.dijkstra import dijkstra
+from search.a_star import a_star
+from search.dijkstra import dijkstra
+from search.breadth_first_search import breadth_first_search as bfs
+from search.depth_first_search import depth_first_search as dfs
 
-
-SIZE: int = 300
-sys.setrecursionlimit(5000)
+SIZE: int = 31
 
 def random_pos() -> tuple[int, int]:
     return (random.randint(0, SIZE - 1), random.randint(0, SIZE - 1))
@@ -24,14 +22,12 @@ def main() -> None:
         end = random_pos()
         print("start:" if i == 0 else "\nstart:", start)
         print("end:", end, end="\n\n")
-
-        print("g_a_star:")
-        print(g_a_star(graph, start, end, cost_1, euc))
-        print("a_star:")
+        print("s_star:")
         print(a_star(graph, start, end, cost_1, euc))
         print("dijkstra:")
         print(dijkstra(graph, start, end, cost_1))
-
+        print("bfs:")
+        print(bfs(graph, start, end, cost_1))
 
 if __name__ == '__main__':
     main()
