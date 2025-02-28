d58f8ee5714a6a4dc59e167d9215bae6e4cd5c40
Cauã Victor Pinheiro da Silva <cauanqx@alu.ufc.br>
2025-02-28 19:39:34 -0300

    Correção leve
    só mudei a ordem da geração das matrizes pra diminuir repetição

--- src/Trabalho 2/main.py
+++ src/Trabalho 2/main.py
@@ -1,7 +1,6 @@
 from functions import *
 import time
 def main():
-    #Aqui nós chamamos a função principal com os parâmetros definidos.
     #Se você quiser testar com diferentes parâmetros, basta mudar os valores aqui
     #Mutações: swap_mutation, scramble_mutation, reverse_mutation
     #Crossovers: order_crossover, pmx_crossover
@@ -11,45 +10,41 @@ def main():
     POPULATION_SIZE = 2000 #Tamanho da população
     EXPERIMENT_1_LENGHT = 10
 
-    flow_matrix = random_flow_matrix(EXPERIMENT_1_LENGHT)
-    distance_matrix = random_distance_matrix(EXPERIMENT_1_LENGHT)
+
     #Parte 1
     #for _ in range (20):
+        #distance_matrix = random_distance_matrix(EXPERIMENT_1_LENGHT) 
+        #flow_matrix = random_flow_matrix(EXPERIMENT_1_LENGHT)
         #genetic_algorithm(POPULATION_SIZE, GENOME_LENGTH, distance_matrix, flow_matrix, tournament_selection, swap_mutation,pmx_crossover,elitism)
         #genetic_algorithm(POPULATION_SIZE, GENOME_LENGTH, distance_matrix, flow_matrix, elitism_selection, swap_mutation,pmx_crossover,elitism)
         # Criando novas matrizes para os próximos testes
+
+    #Parte 2
+    #for _ in range (20):
+        # Criando novas matrizes para os próximos testes
         #distance_matrix = random_distance_matrix(EXPERIMENT_1_LENGHT) 
         #flow_matrix = random_flow_matrix(EXPERIMENT_1_LENGHT)
-    #Parte 2
-    #flow_matrix = random_flow_matrix(EXPERIMENT_1_LENGHT)
-    #distance_matrix = random_distance_matrix(EXPERIMENT_1_LENGHT)
-    #for _ in range (20):
         #genetic_algorithm(POPULATION_SIZE, GENOME_LENGTH, distance_matrix, flow_matrix, tournament_selection, swap_mutation,pmx_crossover,elitism)
         #genetic_algorithm(POPULATION_SIZE, GENOME_LENGTH, distance_matrix, flow_matrix, tournament_selection, swap_mutation,order_crossover,elitism)
 
+
+    #Parte 3
+    #for _ in range (20):
         # Criando novas matrizes para os próximos testes
         #distance_matrix = random_distance_matrix(EXPERIMENT_1_LENGHT) 
         #flow_matrix = random_flow_matrix(EXPERIMENT_1_LENGHT)
-    #Parte 3
-    #flow_matrix = random_flow_matrix(EXPERIMENT_1_LENGHT)
-    #distance_matrix = random_distance_matrix(EXPERIMENT_1_LENGHT)
-    #for _ in range (20):
         #genetic_algorithm(POPULATION_SIZE, GENOME_LENGTH, distance_matrix, flow_matrix, tournament_selection, swap_mutation,pmx_crossover,no_elitism)
         #genetic_algorithm(POPULATION_SIZE, GENOME_LENGTH, distance_matrix, flow_matrix, tournament_selection, swap_mutation,order_crossover,elitism)
 
-        # Criando novas matrizes para os próximos testes
-        #distance_matrix = random_distance_matrix(EXPERIMENT_1_LENGHT) 
-        #flow_matrix = random_flow_matrix(EXPERIMENT_1_LENGHT)
-    #Parte 4
-    #flow_matrix = random_flow_matrix(EXPERIMENT_1_LENGHT)
-    #distance_matrix = random_distance_matrix(EXPERIMENT_1_LENGHT)
-    #for _ in range (20):
-        #genetic_algorithm(POPULATION_SIZE, GENOME_LENGTH, distance_matrix, flow_matrix, tournament_selection, reverse_mutation,pmx_crossover,elitism)
-        #genetic_algorithm(POPULATION_SIZE, GENOME_LENGTH, distance_matrix, flow_matrix, tournament_selection, swap_mutation,order_crossover,elitism)
 
-        # Criando novas matrizes para os próximos testes
-        #distance_matrix = random_distance_matrix(EXPERIMENT_1_LENGHT) 
+    #Parte 4
+    #for _ in range (20):
         #flow_matrix = random_flow_matrix(EXPERIMENT_1_LENGHT)
+        #distance_matrix = random_distance_matrix(EXPERIMENT_1_LENGHT)
+        #genetic_algorithm(POPULATION_SIZE, GENOME_LENGTH, distance_matrix, flow_matrix, tournament_selection, reverse_mutation,pmx_crossover,elitism)
+        #genetic_algorithm(POPULATION_SIZE, GENOME_LENGTH, distance_matrix, flow_matrix, tournament_selection, swap_mutation,order_crossover,elitism)7
+
+        
     #Parte 5
     for i in range (100):
         start_time = time.time()
